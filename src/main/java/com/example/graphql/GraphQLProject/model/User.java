10d3bbc0d78d98154feb3984d4933afabf4aad68
aigulsharip <sharipaigul@gmail.com>
2024-05-22 00:14:52 -0400

    Initial commit of graphql with arrays

---
+++ src/main/java/com/example/graphql/GraphQLProject/model/User.java
@@ -0,0 +1,14 @@
+package com.example.graphql.GraphQLProject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Long id;
+    private String email;
+    private String name;
+}
